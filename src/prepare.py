a1611aea901c81a715bf80b2d3c5cb07f5da9a25
unknown <josrodriguezmo@unal.edu.co>
2023-01-19 07:51:24 -0500

    pipeline training

---
+++ src/prepare.py
@@ -0,0 +1,44 @@
+from dvc import api
+import pandas as pd
+from io import StringIO
+#para el logging usamos sys
+import sys
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s:%(name)s: %(message)s',
+    level=logging.INFO,
+    datefmt='%H:%M:%S',
+    stream=sys.stderr
+)
+
+logger = logging.getLogger(__name__)
+
+logging.info("Fetching data....")
+
+movie_data_path = api.read("dataset/movies.csv", remote="dataset_tracker")
+fin_data_path = api.read("dataset/finantials.csv", remote="dataset_tracker")
+gross_data_path = api.read("dataset/opening_gross.csv", remote="dataset_tracker")
+
+movies_data = pd.read_csv(StringIO(movie_data_path))
+fin_data = pd.read_csv(StringIO(fin_data_path))
+opening_data = pd.read_csv(StringIO(gross_data_path))
+
+
+numeric_columns_mask = (movies_data.dtypes == float) | (movies_data.dtypes == int)
+numeric_columns = [column for column in numeric_columns_mask.index if numeric_columns_mask[column]]
+movies_data = movies_data[numeric_columns + ["movie_title", "cast_total_facebook_likes"]]
+
+
+fin_data = fin_data[['movie_title', 'production_budget', 'worldwide_gross']]
+
+fin_movie_data = pd.merge(fin_data, movies_data, on='movie_title', how='left')
+full_movie_data = pd.merge(opening_data, fin_movie_data, on='movie_title', how='left')
+
+full_movie_data = full_movie_data.drop(['gross','movie_title'], axis=1)
+
+full_movie_data.to_csv('dataset/full_data.csv',index=False)
+
+logger.info('Data Fetched and prepared...')
+
+
